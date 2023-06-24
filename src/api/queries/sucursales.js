5e53d83fe6436562f41a7691b7a8d1ba48ac0010
BerryBGoode <fernandomena3131@gmail.com>
2023-06-23 19:14:23 -0600

    agregar en sucursales - 23/6/2023

--- src/api/queries/sucursales.js
+++ src/api/queries/sucursales.js
@@ -16,4 +16,40 @@ const get = async (req, res) => {
     }
 }
 
-module.exports = { get }
+/**
+ * Método para agregar una sucursal
+ */
+const store = (req, res) => {
+    let er, msg;
+    try {
+        // obtener los datos de la petición
+        const { tel, inicio, cierre, direccion, nit, nrc } = req.body
+        // realizar query 
+        POOL.query('INSERT INTO sucursales(telefono, horario, nrc, nit, direccion) VALUES ($1, $2, $3, $4, $5)',
+            [tel, inicio + ' A.M - ' + cierre + ' P.M', nrc, nit, direccion], (err, result) => {
+                // verificar sí hubo un error                                
+                if (err) {
+
+                    if (err.code === '23505') {
+                        // enviar error el cliente
+                        er = 'Dato unico ya registrado';
+                    } else {
+                        er = err.message;
+                    }
+                    res.json({ error: er });
+                    // sí es ejecuta esto, el status 201 no se enviará
+                    // return;                    
+
+                } else {
+                    msg = 'Sucursal agregada';
+                }
+
+                res.status(201).send(msg);
+            })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Surgio un problema en el servidor')
+    }
+}
+
+module.exports = { get, store }
--- src/api/routes/sucursales.routes.js
+++ src/api/routes/sucursales.routes.js
@@ -1,12 +1,13 @@
 // requerir del enrutador de express
 const { Router } = require('express');
 // requerir de los métodos para realizar los queries
-const { get } = require('../queries/sucursales')
+const { get, store } = require('../queries/sucursales')
 
 // instanciar interface Router
 const SUCURSAL = Router();
 
 SUCURSAL.get('/', get);
+SUCURSAL.post('/', store)
 
 // exportar ruta
 module.exports = SUCURSAL;
