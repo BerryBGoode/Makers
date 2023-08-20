e7f7aeec084881c2436fbee8cf6370176ddc0010
Rober008 <20210722@ricaldone.edu.sv>
2023-08-19 18:23:35 -0600

    report empleados routes

--- src/api/routes/reportes.js
+++ src/api/routes/reportes.js
@@ -2,12 +2,14 @@
 const { Router } = require('express');
 // importando los métodos de /queries/reportes.js
 const { getProxReservaciones } = require('../queries/reportes')
+const { getEmpleadosCargos } = require('../queries/reportes')
 
 // instanciar router para poder crear las rutas del servidor y poder acceder a ellas 
 // del lado del cliente por medio de la url
 const APP = new Router();
 
 APP.get('/proxreservaciones', getProxReservaciones);
+APP.get('/empleadoscargos', getEmpleadosCargos);
 
 // exportar enrutador para importarlo en ../index.js
 module.exports = APP;
