eb4eb5f165d69d4d0c35526e2b1f8102cba6c5da
JonasLening21 <82896130+JonasLening21@users.noreply.github.com>
2023-08-20 14:37:28 -0600

    Queries y routes de Servicios por tipo de servicio (consullta)

--- src/api/queries/reportes.js
+++ src/api/queries/reportes.js
@@ -8,23 +8,6 @@ const { execute } = require('../MySQL')
  * @param {*} req datos de la petición
  * @param {*} res respuesta del servidor
  */
-const getProxReservaciones = async (req, res) => {
-    try {
-        const RESERVACIONES = await execute(`
-        SELECT 
-            concat(c.nombres, ' ', c.apellidos) as cliente, c.dui as duicliente, 
-            concat(e.nombres, ' ', e.apellidos) as empleado, e.dui as duiempleado, 
-            date_format(r.fecha, '%Y-%m-%d') as fecha,
-            TIME_FORMAT(r.hora, '%h:%i') as hora
-        FROM reservaciones r
-        INNER JOIN empleados e ON e.id_empleado = r.id_empleado
-        INNER JOIN clientes c ON c.id_cliente = r.id_cliente
-        WHERE fecha > CURRENT_DATE`);
-        if (res.status(200)) res.json(RESERVACIONES)
-    } catch (error) {
-        res.status(406).send({ error: getError(error) });
-    }
-}
 
 
 const getEmpleadosOrdenes = async (req, res) => {
@@ -44,8 +27,23 @@ const getEmpleadosOrdenes = async (req, res) => {
 }
 
 
+const getTipoServicios = async (req, res) => {
+    try {
+        const SERVICIOS = await execute(`
+        SELECT s.id_servicio, s.nombre_servicio, t.tipo_servicio
+        FROM servicios s
+        INNER JOIN tipos_servicios t ON s.id_tipo_servicio = t.id_tipo_servicio
+        `)
+        if(res.status(200)) res.json(SERVICIOS)
+    } catch (error) {
+        res.status(406).send({ error: getError(error)});
+    }
+}
+
+
+
 
 
 
 // exportando métodos para llamarlo en routes/reportes.routes.js
-module.exports = { getProxReservaciones, getEmpleadosCargos, getProdSucursal, getEmpleadosOrdenes }
+module.exports = { getEmpleadosOrdenes, getTipoServicios }
--- src/api/routes/reportes.routes.js
+++ src/api/routes/reportes.routes.js
@@ -1,19 +1,15 @@
 // importando enrutados de express
 const { Router } = require('express');
 // importando los métodos de /queries/reportes.js
-const { getProxReservaciones } = require('../queries/reportes')
-const { getEmpleadosCargos } = require('../queries/reportes')
-const { getProdSucursal } = require('../queries/reportes')
 const { getEmpleadosOrdenes } = require('../queries/reportes')
+const { getTipoServicios } = require('../queries/reportes')
 
 // instanciar router para poder crear las rutas del servidor y poder acceder a ellas 
 // del lado del cliente por medio de la url
 const APP = new Router();
-
-APP.get('/proxreservaciones', getProxReservaciones);
-APP.get('/empleadoscargos', getEmpleadosCargos);
-APP.get('/prodsucursal', getProdSucursal);
-APP.get('/empleadoOrdene',getEmpleadosOrdenes);
+;
+APP.get('/empleadoOrdenes',getEmpleadosOrdenes);
+APP.get('/tiposervicios', getTipoServicios );
 
 // exportar enrutador para importarlo en ../index.js
 module.exports = APP;
