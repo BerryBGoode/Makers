1a53a58ff095853e19db9b4ae70765914b60607c
BerryBGoode <fernandomena3131@gmail.com>
2023-10-03 23:27:03 -0600

    sidebar tablet semi-responsive

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -18,6 +18,9 @@
     margin: 10% 0;
     width: 120%;
     border-radius: 7px 0 0 7px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
 .li-lista:hover {
@@ -61,31 +64,56 @@
             <li class="li-lista" v-for="(item, index) in this.options" :key="index">
                 <!-- 
               <span class="item">{{ item }}</span> -->
-                <!-- <i>icon</i> -->
-                <router-link class="item" :to="item.route">{{ item.option }} </router-link>
+
+                <router-link v-if="width <= 930" class="item" :to="item.route">
+                    <img :src="item.icon" alt="">
+                </router-link>
+                <router-link v-else class="item" :to="item.route">
+                    <template v-if="width <= 930">
+
+                        <img :src="item.icon" alt="">
+                    </template>
+                    <template v-else>
+                        <span> {{ item.option }} </span>
+
+                    </template>
+                </router-link>
             </li>
         </ul>
     </div>
 </template>
-
 <script>
 // importando componentes para enrutar
 import { RouterLink, RouterView } from 'vue-router';
 import store from '../store';
 import { notificationError } from '../components/alert.vue';
 import axios from 'axios';
+import { mapActions } from 'vuex';
+import { elements } from 'chart.js';
 
 // funcionalidades del componente
 export default {
     // nombre del componente
     name: 'sidebar',
     data() {
+        let width = window.innerWidth;
         return {
             // lista con las optiones que puede acceder el usuario
-            options: [],
+            options: [
+                // { option: 'Inicio', route: '/inicio' },
+                // { option: 'Servicios', route: '/servicios' },
+                // { option: 'Productos', route: '/productos' },
+                // { option: 'Clientes', route: '/clientes' },
+                // { option: 'Empleados', route: '/empleados' },
+                // { option: 'Reservaciones', route: '/reservaciones' },
+                // { option: 'Ordenes', route: '/ordenes' },
+                // { option: 'Sucursales', route: '/sucursales' },
+                // { option: 'Horarios', route: '/horarios' },
+            ],
             logo: '',
             // logomin: './../src/assets/img/logos/logo_blanco_nav.png',
-            cargo: ''
+            cargo: '',
+            width,
         };
     },
     methods: {
@@ -99,16 +127,17 @@ export default {
                     switch (store.state.cargo) {
                         case 'Gerente':
                             this.options = [
-                                { option: 'Inicio', route: '/inicio' },
-                                { option: 'Servicios', route: '/servicios' },
-                                { option: 'Productos', route: '/productos' },
-                                { option: 'Clientes', route: '/clientes' },
-                                { option: 'Empleados', route: '/empleados' },
-                                { option: 'Reservaciones', route: '/reservaciones' },
-                                { option: 'Ordenes', route: '/ordenes' },
-                                { option: 'Sucursales', route: '/sucursales' },
-                                { option: 'Horarios', route: '/horarios' },
-                            ]
+                                { option: 'Inicio', route: '/inicio', text: 'Inicio', icon: './../src/assets/icons/inicio.svg' },
+                                { option: 'Servicios', route: '/servicios', text: 'Servicios', icon: './../src/assets/icons/servicios.svg' },
+                                { option: 'Productos', route: '/productos', text: 'Productos', icon: './../src/assets/icons/productos.svg' },
+                                { option: 'Clientes', route: '/clientes', text: 'Clientes', icon: './../src/assets/icons/clientes.svg' },
+                                { option: 'Empleados', route: '/empleados', text: 'Empleados', icon: './../src/assets/icons/empleados.svg' },
+                                { option: 'Reservaciones', route: '/reservaciones', text: 'Reservaciones', icon: './../src/assets/icons/reservaciones.svg' },
+                                { option: 'Ordenes', route: '/ordenes', text: 'Ordenes', icon: './../src/assets/icons/ordenes.svg' },
+                                { option: 'Sucursales', route: '/sucursales', text: 'Sucursales', icon: './../src/assets/icons/sucursales.svg' },
+                                { option: 'Horarios', route: '/horarios', text: 'Horarios', icon: './../src/assets/icons/horarios.svg' },
+                            ];
+
 
                             break;
 
@@ -154,16 +183,31 @@ export default {
 
             if (width <= 930) {
                 this.options.forEach(elements => {
-                    console.log(elements)
+                    elements.option = elements.icon
+
                 })
             }
+            if (width > 930) {
+                this.options.forEach(elements => {
+                    elements.option = elements.text
+                })
+            }
+
             // eva
+        }
+    },
+    watch: {
+        width(now, old) {
+            console.log(now)
+            console.log(old)
         }
     },
     created() {
         // crear evento cuando se cambia de ancho la pantalla
         window.addEventListener('resize', event => {
             this.validateWindow(document.documentElement.clientWidth)
+            console.log(window.innerWidth)
+            console.log(document.documentElement.clientWidth)
         })
         this.validateWindow(document.documentElement.clientWidth);
     },
